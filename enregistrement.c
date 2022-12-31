1e3905f74550094634ae88170c8e9eb66a7e212f
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2022-12-31 17:52:46 +0100

    Update enregistrement.c

--- enregistrement.c
+++ enregistrement.c
@@ -20,7 +20,7 @@ int enregistrement(int *plateau, int i){
     }
     fprintf(fichier,"\n");
   }
-  
+
   fprintf(fichier,"\n%d",i);
 
 
@@ -29,24 +29,66 @@ int enregistrement(int *plateau, int i){
 
 }
 
+int open_enregistrement(int *plateau){
+  int i ;
+  printf("utiliser sauvgarde \n");
+
+  // Ouvrez un fichier en mode lecture
+  FILE* fichier = fopen("tableau.txt", "r");
+
+  // Vérifiez si l'ouverture du fichier a réussi
+  if (fichier == NULL)
+  {
+      Color(4,0);
+      printf("Impossible d'ouvrir le fichier\n");
+      Color(15,0);
+      return 1;
+  }
+  // Parcourez chaque élément du tableau et lisez-le à partir du fichier
+  for (int i = 0; i < DIMENSION; i++)
+  {
+      for (int j = 0; j < DIMENSION; j++)
+      {
+          fscanf(fichier, "%d", &plateau[i * DIMENSION + j]);
+      }
+  }
+  int compteur = 0;
+  char ligne[256];
+  while (fgets(ligne, sizeof(ligne), fichier) != NULL) // lecture des lignes du fichier
+  {
+    compteur++; // incrémentation du compteur de lignes
+    if (compteur == 3) // si c'est la 3 ligne
+    {
+      sscanf(ligne,"%d", &i); // lecture du nombre à partir de la chaîne de caractères
+      i--;
+      break; // sortie de la boucle
+    }
+  }
+  // Fermez le fichier
+  fclose(fichier);
+  for (int i = 0; i < DIMENSION; i++)
+  {
+      for (int j = 0; j < DIMENSION; j++)
+      {
+          printf("%d ", plateau[i * DIMENSION + j]);
+      }
+      printf("\n");
+  }
+}
+
 /*
-int enregistrement(int *plateau){
-
+int enreeegistrement(int *plateau){
     FILE*fichier = fopen("tableau.txt", "w");
-
     if (fichier == NULL) {
     printf("Impossible d'ouvrir le fichier\n");
     return 1;
     }
 
-    
     fprintf(fichier,"            ");
     for (int i = 0; i < DIMENSION; i++) {
     fprintf(fichier,"%c   ", i  + 48);
     }
     fprintf(fichier,"\n          ");
-
-
     for (int i = 0; i < DIMENSION; i++) {
     fprintf(fichier,"---------------------\n        ");
     fprintf(fichier,"%c |", i+48);
@@ -73,12 +115,7 @@ int enregistrement(int *plateau){
   }
   fprintf(fichier,"---------------------\n\n\n");
 
-
-    
     // Fermeture du fichier
     fclose(fichier);
-
-
 }
-
 */
