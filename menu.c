192220d1825eee9d2d0b2ef9b547c2091f443d05
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2023-01-03 12:39:26 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,6 +1,7 @@
 int menu(int* color_joueur1, int* color_joueur2, int* i, int* dimension, int* nbr_pion, int* nbr_largeur, int* iajoue){
   int couleur, etat_du_jeu =0;
   char touche_menu;
+  system("cls");
   printf("\nBonjour, bien venu dans le menu du jeu du Teeko,\nSi Vous souhaitez:\n - Commencez une nouvelle partie PvP - tapez C\n - Commencez une nouvelle partie PvE - tapez J\n - Ouvrir une partie sauegarde       - tapez S\n - Changer la couleur du");
   Color(*color_joueur1, 0);
   printf(" Joueur 1");
@@ -9,7 +10,7 @@ int menu(int* color_joueur1, int* color_joueur2, int* i, int* dimension, int* nb
   Color(*color_joueur2, 0);
   printf(" Joueur 2");
   Color(15, 0);
-  printf("    - tapez 2\n - Changer la dimension du plateau   - tapez D\n - Changer le nombre de pion         - tapez P\n");
+  printf("    - tapez 2\n - Changer la dimension du plateau   - tapez D\n");
 
   fflush(stdin) ;
   scanf("%c", &touche_menu);
@@ -57,22 +58,6 @@ int menu(int* color_joueur1, int* color_joueur2, int* i, int* dimension, int* nb
   else if(touche_menu == 'S' || touche_menu == 's'){
     etat_du_jeu = 1;
   }
-  else if(touche_menu == 'P' || touche_menu == 'p'){
-    int x; //x est une variable temporaire pour ne pas perdre l'encienne valeur de nbr_pion si la valeur n'est corecte
-    printf("Quel est le nombre de pions souhaitez vous ?\n");
-    fflush(stdin);
-    scanf("%d", &x);
-    if(x < 1){
-      printf("Cette valeur est trop petite, il n'y a pas de pions negatif\n");
-    }
-    else if(x > *nbr_largeur){
-      printf("Cette valeur est trop grande, il ne peux pas y avoir plus de pions que la largeur du plateau\n");
-    }
-    else{
-      *nbr_pion = x;
-    }
-    menu(color_joueur1, color_joueur2, i, dimension, nbr_pion, nbr_largeur, iajoue);
-  }
   else if(touche_menu == 'D' || touche_menu == 'd'){
     int x; //x est une variable temporaire pour ne pas perdre l'encienne valeur de nbr_largeur si la valeur n'est corecte
     printf("Quelle dimension du plateau de jeu souhaitez vous ?\n");
@@ -81,12 +66,9 @@ int menu(int* color_joueur1, int* color_joueur2, int* i, int* dimension, int* nb
     if(x < 1){
       printf("Cette valeur est trop petite, il n'y a pas de taille negatif\n");
     }
-    else if(x < *nbr_pion){
-      printf("Cette valeur est trop petite, il ne peux pas y avoir plus de pions que la largeur du plateau\n");
-    }
+
     else{
       *dimension = x;
-      printf("dimmm%d\n", *dimension);
     }
     menu(color_joueur1, color_joueur2, i, dimension, nbr_pion, nbr_largeur, iajoue);
   }
