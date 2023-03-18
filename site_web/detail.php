f66e8e0ff98dec0a136f5b65193afbc242a5617f
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2023-03-18 01:02:21 +0100

    Update detail.php

--- site_web/detail.php
+++ site_web/detail.php
@@ -2,12 +2,41 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Détail du __ressource__</title>
+        <title>Détail du <?php echo $_GET["type"]; ?></title>
     </head>
     <body>
-        <h1>Détail du __ressource__</h1>
+        <?php $type = $_GET["type"]; $player = 1; $indice_type = 5;?>
+
+        <h1>Détail <?php if ($type =='dollar') {
+            echo 'de tes dollar';$indice_type = 0;}
+          elseif ($type =='electricity') {
+            echo 'de ta production d\'éléctricité';$indice_type = 1;}
+          elseif ($type =='iron') {
+            echo 'de ton fer';$indice_type = 2;}
+          elseif ($type =='oil') {
+            echo 'de ton petrole';$indice_type = 3;}
+          elseif ($type =='uranium') {
+            echo 'de ton uranium';$indice_type = 4;}
+         ?></h1>
+
         <ol>
-            <li>Nombre actuel de __ressource__ : ___</li>
+            <li>Nombre actuel de <?php echo $_GET["type"]; ?> : <?php
+                $liste_ligne = ['nArgent','nElec', 'nFer', 'nPetrole', 'nUranium'];
+                try
+                {
+                    $bdd = new PDO("mysql:host=localhost;port=3306;dbname=projet_energie;charset=utf8", "root", "");  // lier la base de donnees, a mon avis pas besoin de le mettre tt le temps
+                }
+                catch (Exception $e)
+                {
+                    echo 'la base de donnée n\'a pas pu etre chargé';
+                    die('Erreur : ' . $e->getMessage()); // si ca marche pas ca dit qu'il y a une erreur
+                }
+                $req = $bdd->prepare("SELECT $liste_ligne[$indice_type] FROM joueur WHERE idJoueur = $player"); // req contient une requette SQL
+                $req->execute(); // On l'execute
+                $donnees = $req->fetch(); //On transforme ca en donnees exploitable
+                echo $donnees[$liste_ligne[$indice_type]];
+
+            ?></li>
             <ol>
                 <li>Mine/Centrales : +___</li>
                 <li>Contrats : +___</li>
