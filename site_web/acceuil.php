60283f775d80e8db22de76d6071da522e2f6df9c
matthieu777 <70650109+matthieu777@users.noreply.github.com>
2023-03-29 19:19:01 +0200

    Add files via upload

---
+++ site_web/acceuil.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/acceuil.css">
+</head>
+<body>
+    
+    <div class="nav-barre">
+        <div class="nav-barre-gauche">
+            logo
+        </div>
+        <div class="nav-barre-centre">
+            <h1 class="nav-barre-titre"><u> Acceuil </u><h1>
+        </div>
+        <div class="nav-barre-droite">
+            <div class="nav-barre-droite-gauche">
+                <button class="nav-barre-boutton" onclick="window.location.href='inscription.php'">
+                    <span>Inscription</span>
+                </button>
+            </div>
+            <div class="nav-barre-droite-droite">
+            <button class="nav-barre-boutton" onclick="window.location.href='connexion.php'">
+                    <span>Connexion</span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="body-acceuil">
+
+        <div class="body-acceuil-gauche">
+
+                <h2 class="body-acceuil-gauche-titre">Bienvenue sur « …».</h2>
+
+                <p class="body-acceuil-gauche-texte">Farmer de l'Energie, des<br> Ressources et de l'Argent<br> et trader les avec vos amis.<br><br>Tenter de devenir le boss<br> de l'Energie.</p>
+
+        </div>
+
+        <div class="body-acceuil-droite">
+            <div class="body-acceuil-droite-box-image">
+                <p>mettre image</p>
+                <button onclick="window.location.href='main.php'">
+                    <span>temporaire pour aller direct au jeu</span>
+                </button>
+            </div>
+        </div>
+        
+    </div>
+
+</body>
+</html>
