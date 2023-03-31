9f3cf0a973dd6b03c3431639c082791bbea7387a
matthieu777 <70650109+matthieu777@users.noreply.github.com>
2023-03-31 17:48:11 +0200

    Add files via upload

---
+++ site_web/inscription.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/inscription.css">
+</head>
+<body>
+    
+    <div class="nav-barre">
+        <div class="nav-barre-gauche">
+            logo
+        </div>
+        <div class="nav-barre-centre">
+            <h1 class="nav-barre-titre"><u> Inscription</u><h1>
+        </div>
+        <div class="nav-barre-droite">
+            <div class="nav-barre-droite-gauche">
+                <button class="nav-barre-boutton" onclick="window.location.href='acceuil.php'">
+                    <span>Acceuil</span>
+                </button>
+            </div>
+            <div class="nav-barre-droite-droite">
+            <button class="nav-barre-boutton" onclick="window.location.href='connexion.php'">
+                    <span>Connexion</span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="body-inscription">
+        <div class = "contour">
+        
+        </div>
+
+    </div>
+
+</body>
+</html>
