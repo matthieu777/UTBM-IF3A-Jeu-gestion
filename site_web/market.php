0aff26a7cef1eb26cc523f0573a86cc6c0951714
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2023-04-07 17:13:22 +0200

    Update market.php

--- site_web/market.php
+++ site_web/market.php
@@ -8,24 +8,13 @@
         <h1>Market Place</h1>
         <p>
         <?php
-        try
-        {
-            $bdd = new PDO("mysql:host=localhost;port=3306;dbname=projet_energie;charset=utf8", "root", "");  // lier la base de donnees, a mon avis pas besoin de le mettre tt le temps
-        }
-        catch (Exception $e)
-        {
-            echo 'la base de donnée n\'a pas pu etre chargé';
-            die('Erreur : ' . $e->getMessage()); // si ca marche pas ca dit qu'il y a une erreur
-        }
-        $req = $bdd->prepare("SELECT COUNT(*) FROM `contrat`"); // req contient une requette SQL
-        $req->execute(); // On l'execute
-        $nbr_contrat = $req->fetch(); //On transforme ca en donnees exploitable
+        include("function_for_bdd.php");
+
+        $nbr_contrat = executeSQLRequest("SELECT COUNT(*) FROM `contrat`", array());
         $nbr_contrat = $nbr_contrat['COUNT(*)'];
 
         for ($i=1; $i < $nbr_contrat +1 ; $i++) {
-            $req = $bdd->prepare("SELECT idVendeur, speudo , ressource1, valeur1, ressource2, valeur2 FROM contrat INNER JOIN joueur ON idVendeur = joueur.idJoueur WHERE idContrat = $i"); // req contient une requette SQL
-            $req->execute(); // On l'execute
-            $donnees = $req->fetch();
+            $donnees = executeSQLRequest("SELECT idVendeur, speudo , ressource1, valeur1, ressource2, valeur2 FROM contrat INNER JOIN joueur ON idVendeur = joueur.idJoueur WHERE idContrat = $i", array());
             echo $donnees['speudo'], ' veut vendre ', $donnees['valeur1'],' ', $donnees['ressource1'], ' en echange de ', $donnees['valeur2'],' ', $donnees['ressource2']; ?><br><?php
           }?>
 
