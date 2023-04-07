e64b09b86164c8eca2f83f45a88140756282144e
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2023-04-07 17:13:49 +0200

    Update detail.php

--- site_web/detail.php
+++ site_web/detail.php
@@ -5,7 +5,9 @@
         <title>Détail du <?php echo $_GET["type"]; ?></title>
     </head>
     <body>
-        <?php $type = $_GET["type"]; $player = 1; $indice_type = 5;?>
+        <?php
+        include("function_for_bdd.php");
+        $type = $_GET["type"]; $player = 1; $indice_type = 5;?>
 
         <h1>Détail <?php if ($type =='dollar') {
             echo 'du gain d\'argent';$indice_type = 0;}
@@ -22,19 +24,10 @@
         <ol>
             <li>Nombre actuel de <?php echo $_GET["type"]; ?> : <?php
                 $liste_ligne = ['nArgent','nElec', 'nFer', 'nPetrole', 'nUranium'];
-                try
-                {
-                    $bdd = new PDO("mysql:host=localhost;port=3306;dbname=projet_energie;charset=utf8", "root", "");  // lier la base de donnees, a mon avis pas besoin de le mettre tt le temps
-                }
-                catch (Exception $e)
-                {
-                    echo 'la base de donnée n\'a pas pu etre chargé';
-                    die('Erreur : ' . $e->getMessage()); // si ca marche pas ca dit qu'il y a une erreur
-                }
-                $req = $bdd->prepare("SELECT $liste_ligne[$indice_type] FROM joueur WHERE idJoueur = $player"); // req contient une requette SQL
-                $req->execute(); // On l'execute
-                $donnees = $req->fetch(); //On transforme ca en donnees exploitable
-                echo $donnees[$liste_ligne[$indice_type]];
+
+                echo executeSQLRequest("SELECT $liste_ligne[$indice_type] FROM joueur WHERE idJoueur = $player", array());
+
+
 
             ?></li>
             <ol>
@@ -48,3 +41,4 @@
         </ol>
     </body>
 </html>
+
