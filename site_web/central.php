7c51aed502a0800abca938e5cdf92a8985a0c344
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-04-14 17:43:45 +0200

    Update central.php

--- site_web/central.php
+++ site_web/central.php
@@ -6,6 +6,10 @@
     </head>
     <body>
         <?php
+            $type = $_GET["type"];
+            $name = $_GET["name"];
+            $datecrea = $_GET["datecrea"];
+
             $random_sentence_pplt = [
                 "wind_turbine" => [
                     '"Le vent souffle sur les plaines de la Bretagne Armoricaine."',
@@ -26,36 +30,47 @@
                     'Au soleil, m'."'".'exposer un peu plus au soleil.'
                 ]
             ];
-            $type = $_GET["type"];
+
             switch ($type) {
                 case 'wind_turbine':
-                    echo "<h1>Eolienne</h1>";
+                    echo "<h1>$name (Eolienne)</h1>";
                     break;
                 case 'oil_power_station':
-                    echo "<h1>Centrale à pétrole</h1>";
+                    echo "<h1>$name (Centrale à pétrole)</h1>";
                     break;
                 case 'nuclear_plant':
-                    echo "<h1>Centrale nucléaire</h1>";
+                    echo "<h1>$name (Centrale nucléaire)</h1>";
                     break;
                 case 'dam':
-                    echo "<h1>Barrage hydroélectrique</h1>";
+                    echo "<h1>$name (Barrage hydroélectrique)</h1>";
                     break;
                 case 'solar_panel':
-                    echo "<h1>Panneau solaire</h1>";
+                    echo "<h1>$name (Panneau solaire)</h1>";
+                    break;
+                case 'iron':
+                    echo "<h1>$name (Mine de fer)</h1>";
+                    break;
+                case 'oil':
+                    echo "<h1>$name (Pompes à pétroles)</h1>";
+                    break;
+                case 'uranium':
+                    echo "<h1>$name (Mine d'uranium)</h1>";
                     break;
                 default:
-                    echo "<h1>Undefined Structure</h1>";
+                    echo "<h1>$name (Undefined Structure)</h1>";
                     break;
             }
-        ?>
 
-        <h2>Rendement : ___ WhattMois</h2>
-        <h2>Prix de vente : ___ $</h2>
-        <h2>Date de création : __/__/____</h2>
 
-        <?php
-            $i = rand(0, count($random_sentence_pplt[$type])-1);
-            echo '<h3>'.$random_sentence_pplt[$type][$i].'</h3>';
+            echo "<h2>Rendement : __ WhattMois</h2>";
+            echo "<h2>Prix de vente : __ $</h2>";
+            echo "<h2>Date de création (en tour) : $datecrea</h2>";
+
+            if ($type != "iron" and $type != "oil" and $type != "uranium") {
+                $i = rand(0, count($random_sentence_pplt[$type])-1);
+                echo '<h3>'.$random_sentence_pplt[$type][$i].'</h3>';
+            }
+
         ?>
 
         <button type="button" class="sell">Vendre</button>
