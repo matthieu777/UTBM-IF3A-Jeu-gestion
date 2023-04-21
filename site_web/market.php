90eca8ad372ce973c75329692ab46a0f45436d3c
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2023-04-21 17:39:25 +0200

    Update market.php

--- site_web/market.php
+++ site_web/market.php
@@ -4,22 +4,28 @@
         <meta charset="utf-8">
         <title>Market Place</title>
     </head>
+
     <body>
         <h1>Market Place</h1>
         <p>
         <?php
         include("function_for_bdd.php");
 
-        $nbr_contrat = executeSQLRequest("SELECT COUNT(*) FROM `contrat`", array());
-        $nbr_contrat = $nbr_contrat['COUNT(*)'];
-
+        $nbr_contrat = requestResultToArray(executeSQLRequest("SELECT ? FROM `contrat`", array('COUNT(*)')));
+        $nbr_contrat = count($nbr_contrat);
+        //parcours au temps de fois qu'il y ai de contrat pour tous les afficher
         for ($i=1; $i < $nbr_contrat +1 ; $i++) {
-            $donnees = executeSQLRequest("SELECT idVendeur, speudo , ressource1, valeur1, ressource2, valeur2 FROM contrat INNER JOIN joueur ON idVendeur = joueur.idJoueur WHERE idContrat = $i", array());
-            echo $donnees['speudo'], ' veut vendre ', $donnees['valeur1'],' ', $donnees['ressource1'], ' en echange de ', $donnees['valeur2'],' ', $donnees['ressource2']; ?><br><?php
+            $donnees = executeSQLRequest("SELECT idVendeur, pseudo , ressource1, valeur1, ressource2, valeur2 FROM contrat INNER JOIN joueur ON idVendeur = joueur.idJoueur WHERE idContrat = $i", array());
+            $donnees = $donnees->fetch();
+            //affichage de la ligne de contrat
+            echo $donnees['pseudo'], ' veut vendre ', $donnees['valeur1'],' ', $donnees['ressource1'], ' en echange de ', $donnees['valeur2'],' ', $donnees['ressource2']; ?><br><?php
           }?>
 
       </p>
-
-        <h3>Il n'y a aucun contrat pour le moment.</h3>
+      <?php // si il n'y a pas de contrat
+      if($nbr_contrat == 0){
+        ?>
+        <h3>Il n'y a aucun contrat pour le moment.</h3><?php
+      }?>
     </body>
 </html>
