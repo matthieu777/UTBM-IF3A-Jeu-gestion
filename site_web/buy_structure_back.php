6394f2c090440e4ce5bb9643abf32864d7882d2a
PierreGuerout <117822236+PierreGuerout@users.noreply.github.com>
2023-04-25 13:30:01 +0200

    Update buy_structure_back.php

--- site_web/buy_structure_back.php
+++ site_web/buy_structure_back.php
@@ -4,7 +4,7 @@
     $type = $_GET["type"];
     $playerId = $_GET["playerid"];
 
-    $r = "INSERT INTO `structure` (`idProprietaire`, `type`, `nom`, `dateCreation`) VALUES (?, ?, 'Eolienne 1', '0');";
+    $r = "INSERT INTO `structure` (`idProprietaire`, `type`, `nom`, `tourCreation`) VALUES (?, ?, 'Eolienne 1', '0');";
     executeSQLRequest($r, array("$playerId", "$type"));
 
     header("Location: buy_structure_front.php?type=".$type."&playerid=".$playerId);
