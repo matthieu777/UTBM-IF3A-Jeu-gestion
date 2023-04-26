bda975e8d139cdba6732411b0664ae9ecc08bcd2
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-04-26 19:26:32 +0200

    Update market.php

--- site_web/market.php
+++ site_web/market.php
@@ -15,28 +15,28 @@
         <h1>Market Place</h1>
         <p>
         <?php
-        include("function_for_bdd.php");
+            include("function_for_bdd.php");
 
-        $nbr_contrat = requestResultToArray(executeSQLRequest("SELECT ? FROM `contrat`", array('COUNT(*)')));
-        $nbr_contrat = count($nbr_contrat);
-        //parcours au temps de fois qu'il y ai de contrat pour tous les afficher
-        for ($i=1; $i < $nbr_contrat +1 ; $i++) {
-            $donnees = executeSQLRequest("SELECT idVendeur, pseudo , ressource1, valeur1, ressource2, valeur2 FROM contrat INNER JOIN joueur ON idVendeur = joueur.idJoueur WHERE idContrat = ?", array($i));
-            $donnees = $donnees->fetch();
-            //affichage de la ligne de contrat
-            $button_names = ['dollar', 'electricity', 'iron', 'oil', 'uranium'];
+            $id_contrat = requestResultToArray(executeSQLRequest("SELECT idContrat FROM `contrat`", array()));
 
-            echo '<button name = '.$i.' type="button_marcket"> '.$donnees['pseudo'].' veut vendre '.$donnees['valeur1'].' <img src="textures/'.$donnees['ressource1'].'.png"> en echange de '.$donnees['valeur2'].' <img src="textures/'.$donnees['ressource2'].'.png">
-
-            </button>'
-            //echo $donnees['pseudo'], ' veut vendre ', $donnees['valeur1'],' ', $donnees['ressource1'], ' en echange de ', $donnees['valeur2'],' ', $donnees['ressource2']; ?><br><?php
-          }?>
-
-      </p>
-      <?php // si il n'y a pas de contrat
-      if($nbr_contrat == 0){
+            //parcours autant de fois qu'il y a de contrat pour tous les afficher
+            for ($i = 0; $i < count($id_contrat); $i++) {
+                $donnees = executeSQLRequest("SELECT idVendeur, pseudo , ressource1, valeur1, ressource2, valeur2 FROM contrat INNER JOIN joueur ON idVendeur = joueur.idJoueur WHERE idContrat = ?", array($id_contrat[$i][0]));
+                $res = requestResultToArray($donnees);
+                if (count($res) != 0){
+                    //affichage de la ligne de contrat
+                    echo '<button name = '.$i.' type="button_market"> '.$res[0][1].' veut vendre '.$res[0][3].' <img src="textures/'.$res[0][2].'.png"> en echange de '.$res[0][5].' <img src="textures/'.$res[0][4].'.png">
+                    </button>';
+                }
+            }
         ?>
-        <h3>Il n'y a aucun contrat pour le moment.</h3><?php
-      }?>
+        </p>
+
+        <?php // si il n'y a pas de contrat
+          if(count($id_contrat) == 0){
+            echo "<h3>Il n'y a aucun contrat pour le moment.</h3>";
+          }
+        ?>
+
     </body>
 </html>
