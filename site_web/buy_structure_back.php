05a443cc493373f15b1a51aac9e18c09e4a21562
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-04-26 11:30:43 +0200

    Update buy_structure_back.php

--- site_web/buy_structure_back.php
+++ site_web/buy_structure_back.php
@@ -5,8 +5,42 @@
     $playerId = $_GET["playerid"];
     $tour = $_GET["tour"];
 
-    $r = "INSERT INTO `structure` (`idProprietaire`, `type`, `nom`, `tourCreation`) VALUES (?, ?, 'Eolienne 1', ?);";
-    executeSQLRequest($r, array($playerId, $type, $tour));
+    switch ($type) {
+        case 'wind_turbine':
+            $name = "Eolienne";
+            break;
+        case 'oil_power_station':
+            $name = "Centrale à pétrole";
+            break;
+        case 'nuclear_plant':
+            $name = "Centrale nucléaire";
+            break;
+        case 'dam':
+            $name = "Barrage hydroélectrique";
+            break;
+        case 'solar_panel':
+            $name = "Panneau solaire";
+            break;
+        case 'iron':
+            $name = "Mine de fer";
+            break;
+        case 'oil':
+            $name = "Pompes à pétrole";
+            break;
+        case 'uranium':
+            $name = "Mine d'uranium";
+            break;
+        default:
+            $name = "Undefined";
+            break;
+    }
+
+    $r = "SELECT COUNT(*) + 1 FROM structure WHERE structure.type = ?";
+    $num = requestResultToArray(executeSQLRequest($r, array($type)))[0][0];
+    $name = $name." ".$num;
+
+    $r = "INSERT INTO `structure` (`idProprietaire`, `type`, `nom`, `tourCreation`) VALUES (?, ?, ?, ?);";
+    executeSQLRequest($r, array($playerId, $type, $name, $tour));
 
 ?>
 <script>
