4ef8dc022608e08a9804bdd09e1c4419e897b34f
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-04-26 10:40:57 +0200

    Update buy_structure_back.php

--- site_web/buy_structure_back.php
+++ site_web/buy_structure_back.php
@@ -3,9 +3,10 @@
 
     $type = $_GET["type"];
     $playerId = $_GET["playerid"];
+    $tour = $_GET["tour"];
 
-    $r = "INSERT INTO `structure` (`idProprietaire`, `type`, `nom`, `tourCreation`) VALUES (?, ?, 'Eolienne 1', '0');";
-    executeSQLRequest($r, array("$playerId", "$type"));
+    $r = "INSERT INTO `structure` (`idProprietaire`, `type`, `nom`, `tourCreation`) VALUES (?, ?, 'Eolienne 1', ?);";
+    executeSQLRequest($r, array($playerId, $type, $tour));
 
 ?>
 <script>
