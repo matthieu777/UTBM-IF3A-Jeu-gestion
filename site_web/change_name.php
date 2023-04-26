e4379cd7f76022f61a46222b5f8f2aa2251fe2c0
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-04-26 11:31:54 +0200

    Add files via upload

---
+++ site_web/change_name.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="post">
+            <p>Nouveau nom : <input type="text" name="name" value=""></p>
+            <input type="submit" name="submit" value="Changer de nom">
+        </form>
+    </body>
+</html>
