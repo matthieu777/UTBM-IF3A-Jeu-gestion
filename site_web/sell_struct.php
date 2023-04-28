a180204a47d0873358eebe3217df30752e2c403d
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-04-28 17:34:29 +0200

    Update sell_struct.php

--- site_web/sell_struct.php
+++ site_web/sell_struct.php
@@ -1,14 +1,16 @@
 <?php
     include("function_for_bdd.php");
 
-    $type = $_GET["type"];
-    $name = $_GET["name"];
     $id = $_GET['id'];
-    $datecrea = $_GET["datecrea"];
+    $price = $_GET['price'];
+
+    // Ajout à la base de donnée de l'argent gagné lors de la vente
+    $r = "UPDATE joueur SET numeroArgent = numeroArgent + ? WHERE idJoueur = (SELECT idProprietaire FROM structure WHERE idStructure = ?)";
+    executeSQLRequest($r, array($price, $id));
 
     $r = "DELETE FROM structure WHERE idStructure = ?";
     executeSQLRequest($r, array($id));
 ?>
 <script>
-    top.window.location = 'main.php';
+  top.window.location = 'main.php';
 </script>
