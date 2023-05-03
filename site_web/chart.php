11c4f9dd5efe573fc988536a8a104c7660f475cc
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-05-03 16:22:05 +0200

    Update chart.php

--- site_web/chart.php
+++ site_web/chart.php
@@ -2,14 +2,21 @@
 	include("function_for_bdd.php");
 
 	$idPlayer = $_GET["idPlayer"];
-	$r = "SELECT tour, valeur FROM `graphoffre` WHERE idJoueur = ?";
-	$points = requestResultToArray(executeSQLRequest($r, array($idPlayer)));
 
-	$dataPointsEnergy = array();
+	$r_offre = "SELECT tour, valeur FROM `graphoffre` WHERE idJoueur = ?";
+	$r_demande = "SELECT tour, valeur FROM `graphdemande` WHERE partie = (SELECT numeroPartie FROM joueur WHERE idJoueur = ?)";
+	$points_offre = requestResultToArray(executeSQLRequest($r_offre, array($idPlayer)));
+	$points_demande = requestResultToArray(executeSQLRequest($r_demande, array($idPlayer)));
 
-	for ($i=0; $i < count($points); $i++) {
-		$arr = array('x' => $points[$i][0], 'y' => $points[$i][1]);
-		array_push($dataPointsEnergy, $arr);
+	$dataPointsEnergyProduced = array();
+	$dataPointsEnergyAsked = array();
+
+	for ($i=0; $i < count($points_offre); $i++) {
+		$arr = array('x' => $points_offre[$i][0], 'y' => $points_offre[$i][1]);
+		array_push($dataPointsEnergyProduced, $arr);
+
+		$arr = array('x' => $points_demande[$i][0], 'y' => $points_demande[$i][1]);
+		array_push($dataPointsEnergyAsked, $arr);
 	}
 ?>
 
@@ -20,34 +27,46 @@
     <meta charset="UTF-8">
     <script>
         window.onload = function () {
-
-            var chart = new CanvasJS.Chart("chartContainer", {
-            	animationEnabled: false,
+			var chart = new CanvasJS.Chart("chartContainer", {
+				animationEnabled: false,
 				backgroundColor: "rgba(214, 217, 217, 0.3)",
-
-            	title:{
-            		text: "Courbe d'offre de l'énergie"
-
-            	},
-            	axisX:{
-            		title: "Mois"
-            	},
-            	axisY:{
-            		title: "WhattMois",
-            		titleFontColor: "#402813",
+				title:{
+					text: "Courbes"
+				},
+				axisX:{
+					title: "Temps (Mois)"
+				},
+				axisY:{
+					title: "Production (WhattMois)",
+					titleFontColor: "#402813",
             		lineColor: "#402813",
             		labelFontColor: "#402813",
             		tickColor: "#402813"
-            	},
-            	data: [{
-            		type: "line",
-            		name: "Energie",
-            		markerSize: 0,
-            		toolTipContent: "Mois: {x} <br>{name}: {y} WhattMois",
-            		dataPoints: <?php echo json_encode($dataPointsEnergy, JSON_NUMERIC_CHECK); ?>
-            	}]
-
-            });
+				},
+				axisY2:{
+					title: "Demande (WhattMois)",
+					titleFontColor: "#001545",
+            		lineColor: "#001545",
+            		labelFontColor: "#001545",
+            		tickColor: "#001545"
+				},
+				data: [{
+					type: "line",
+					name: "Energie produite",
+					markerSize: 0,
+					toolTipContent: "Temperature: {x} °C <br>{name}: {y} mPa.s",
+					showInLegend: true,
+					dataPoints: <?php echo json_encode($dataPointsEnergyProduced, JSON_NUMERIC_CHECK); ?>
+				},{
+					type: "line",
+					axisYType: "secondary",
+					name: "Energie demandée",
+					markerSize: 0,
+					toolTipContent: "Temperature: {x} °C <br>{name}: {y} g/cm³",
+					showInLegend: true,
+					dataPoints: <?php echo json_encode($dataPointsEnergyAsked, JSON_NUMERIC_CHECK); ?>
+				}]
+			});
             chart.render();
 
         }
