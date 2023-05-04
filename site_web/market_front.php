f81ef3095b61c77d840a11f3a0f462c82e804032
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-05-04 14:45:56 +0200

    Update and rename market.php to market_front.php

--- site_web/market_front.php
+++ site_web/market_front.php
@@ -17,6 +17,8 @@
         <?php
             include("function_for_bdd.php");
 
+            $idPlayer = $_GET["idPlayer"];
+
             $id_contrat = requestResultToArray(executeSQLRequest("SELECT idContrat FROM `contrat`", array()));
 
             //parcours autant de fois qu'il y a de contrat pour tous les afficher
@@ -25,8 +27,7 @@
                 $res = requestResultToArray($donnees);
                 if (count($res) != 0){
                     //affichage de la ligne de contrat
-                    echo '<button name = '.$i.' type="button_market"> '.$res[0][1].' veut vendre '.$res[0][3].' <img src="textures/'.$res[0][2].'.png"> en echange de '.$res[0][5].' <img src="textures/'.$res[0][4].'.png">
-                    </button>';
+                    echo '<button name = '.$i.' type="button_market" OnClick="GoToBack('.$id_contrat[$i][0].','.$idPlayer.')"> '.$res[0][1].' veut vendre '.$res[0][3].' <img src="textures/'.$res[0][2].'.png"> en echange de '.$res[0][5].' <img src="textures/'.$res[0][4].'.png"></button>';
                 }
             }
         ?>
@@ -40,3 +41,8 @@
 
     </body>
 </html>
+<script type="text/javascript">
+    function GoToBack(idContrat, idPlayer){
+        window.location = "market_back.php?idContrat=" + idContrat + "&idPlayer=" + idPlayer;
+    }
+</script>
