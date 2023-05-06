409b7bed6310fabd276e97034e65904a7637777c
Jouibhb <118623593+Jouibhb@users.noreply.github.com>
2023-05-06 10:21:43 +0200

    Update market_back.php

--- site_web/market_back.php
+++ site_web/market_back.php
@@ -2,14 +2,6 @@
     $idContrat = $_GET["idContrat"];
     $idPlayer = $_GET["idPlayer"];
 
-    include("function_for_bdd.php");
-
-    $r = "SELECT idVendeur, ressource1, valeur1, ressource2, valeur2 FROM contrat WHERE idContrat = ?";
-    $res = requestResultToArray(executeSQLRequest($r, array($idContrat)));
-
-    $r = "DELETE FROM contrat WHERE idContrat = ?";
-    executeSQLRequest($r, array($idContrat));
-
     $arr = [
         "dollar" => "numeroArgent",
         "electricity" => "nombreElec",
@@ -18,9 +10,22 @@
         "uranium" => "nombreUranium"
     ];
 
-    $r = "UPDATE joueur SET ".$arr[$res[0][1]]." = ".$arr[$res[0][1]]." + ?, ".$arr[$res[0][3]]." = ".$arr[$res[0][3]]." - ? WHERE idJoueur = ?";
-    executeSQLRequest($r, array(intval($res[0][2]), intval($res[0][4]), $idPlayer));
+    include("function_for_bdd.php");
 
-    $r = "UPDATE joueur SET ".$arr[$res[0][3]]." = ".$arr[$res[0][3]]." + ?, ".$arr[$res[0][1]]." = ".$arr[$res[0][1]]." - ? WHERE idJoueur = ?";
-    executeSQLRequest($r, array(intval($res[0][4]), intval($res[0][2]), $res[0][0]));
+    $r = "SELECT idVendeur, ressource1, valeur1, ressource2, valeur2 FROM contrat WHERE idContrat = ?";
+    $res = requestResultToArray(executeSQLRequest($r, array($idContrat)));
+
+    $r = "SELECT numeroArgent, nombreFer, nombrePetrole, nombreUranium FROM joueur WHERE idJoueur = ?";
+    $ressources = executeSQLRequest($r, array($playerId))->fetch();
+
+    if($ressources[$arr[$res[0][1]]] >= $res[0][2] and $ressources[$arr[$res[0][3]]] >= $res[0][4]){
+        $r = "DELETE FROM contrat WHERE idContrat = ?";
+        executeSQLRequest($r, array($idContrat));
+
+        $r = "UPDATE joueur SET ".$arr[$res[0][1]]." = ".$arr[$res[0][1]]." + ?, ".$arr[$res[0][3]]." = ".$arr[$res[0][3]]." - ? WHERE idJoueur = ?";
+        executeSQLRequest($r, array(intval($res[0][2]), intval($res[0][4]), $idPlayer));
+
+        $r = "UPDATE joueur SET ".$arr[$res[0][3]]." = ".$arr[$res[0][3]]." + ?, ".$arr[$res[0][1]]." = ".$arr[$res[0][1]]." - ? WHERE idJoueur = ?";
+        executeSQLRequest($r, array(intval($res[0][4]), intval($res[0][2]), $res[0][0]));
+    }
 ?>
